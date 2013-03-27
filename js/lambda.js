aeac97437c856d08a5bc545c966b5701004d82d0
Ryan McGowan <ryan@ryanmcg.com>
2013-03-26 21:20:27 -0400

    Implement reduce and fix map.

--- js/lambda.js
+++ js/lambda.js
@@ -4,11 +4,55 @@
   λ.map = function (coll, func) {
     var output = [];
     for (var index in coll) {
-      output.push(func(coll[index]));
+      if (coll.hasOwnProperty(index)) {
+        output.push(func(coll[index]));
+      }
     }
     return output;
   };
 
-  λ.reduce = function (coll, func) {
+  λ.reduce = function (coll, func, memo) {
+    for (var index in coll) {
+      if (coll.hasOwnProperty(index)) {
+        if (memo === undefined) {
+          memo = coll[index];
+        } else {
+          memo = func(memo, coll[index], index);
+        }
+      }
+    }
+    return memo;
   };
+
+  context.λ = λ;
+  context.L = λ;
 })(this);
+
+
+var inc = function (v) {
+  return v + 1;
+};
+
+var product = function (pro, next) {
+  return pro * next;
+};
+
+
+assert(equalToString(λ.map([1,2,3], inc), [2,3,4]), "Mappin'");
+assert(equal(λ.reduce([2,3,4], product), 24), "Reducin'");
+assert(equal(λ.reduce([2,3,4], product, 3), 72), "Reducin' with memo");
+
+function equal(v1, v2) {
+  return v1 === v2;
+}
+
+function equalToString(v1, v2) {
+  return v1.toString() === v2.toString();
+}
+function assert(value, message) {
+  if (!value) {
+    throw "FAILED: " + message;
+  } else {
+    console.log(message + " works!");
+  }
+}
